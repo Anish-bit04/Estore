0579b6abb0b9a5e9978f44e6741e568c20bbb019
Anish-dev <113178810+DarkPhoenix002@users.noreply.github.com>
2023-05-16 01:07:51 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,20 @@ import Home from './component/Home'
 import Cart from './component/Cart'
 import { Provider } from 'react-redux'
 import store from './store/store'
-import Header from './component/Header'
+import Navbar from './component/Navbar'
 
 const App = () => {
   return (
     <div>
-    <Provider store={store}>
-    <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-      </Routes>
-      </BrowserRouter>
-    </Provider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar/>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/cart' element={<Cart />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </div>
   )
 }
